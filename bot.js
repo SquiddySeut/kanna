6a2611bf1f5149419d116c8d38588b2f89d212b3
SquiddySeut <40527468+SquiddySeut@users.noreply.github.com>
2018-06-26 13:33:35 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+//var qrkannabed = document.createElement("img");
+//qrkannabed.src = "kannabedpic.jpeg";
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -22,8 +24,12 @@ client.on('message', message => {
     	message.reply('hey um...');
         message.reply('if you really want to see a naked body,');
         message.channel.send("You can look at my body..."); 
+        message.reply("http://ae01.alicdn.com/kf/HTB14_hCiMnH8KJjSspcq6z3QFXaL/Miss-Kobayashi-s-Dragon-Mai-Kanna-Kamui-Hug-Body-Pillow-Cover-Tohru-Kobayashi-Dakimakura-Case.jpg");
   	}
 });
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
+//http://ae01.alicdn.com/kf/HTB14_hCiMnH8KJjSspcq6z3QFXaL/Miss-Kobayashi-s-Dragon-Mai-Kanna-Kamui-Hug-Body-Pillow-Cover-Tohru-Kobayashi-Dakimakura-Case.jpg
+//ae01.alicdn.com/kf/HTB14_hCiMnH8KJjSspcq6z3QFXaL/Miss-Kobayashi-s-Dragon-Mai-Kanna-Kamui-Hug-Body-Pillow-Cover-Tohru-Kobayashi-Dakimakura-Case.jpg
+//<img src="https://ae01.alicdn.com/kf/HTB14_hCiMnH8KJjSspcq6z3QFXaL/Miss-Kobayashi-s-Dragon-Mai-Kanna-Kamui-Hug-Body-Pillow-Cover-Tohru-Kobayashi-Dakimakura-Case.jpg" alt="Image result for kanna kamui sexy"/>
